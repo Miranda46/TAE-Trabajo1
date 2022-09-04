7994ccc6d8ae5b6c21b65d6d9c9e35e5ccdaea39
EsGarciaC <esgarciac@unal.edu.co>
2022-09-04 15:30:21 -0500

    Update TAE_streamlit.py

--- TAE_streamlit.py
+++ TAE_streamlit.py
@@ -6,7 +6,20 @@ DATA_CSV = "CollegeScorecard.csv"
 
 @st.cache
 def load_data():
-    data = pd.read_csv(DATA_CSV, )
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
+    data = pd.read_csv(DATA_CSV)
+
+    data = data[["UNITID", "INSTNM", "LATITUDE", "LONGITUDE",  "DEP_INC_AVG", "IND_INC_AVG", "GRAD_DEBT_MDN", "CONTROL"]]
+    #data["DEP_INC_AVG"] = data["DEP_INC_AVG"].astype(float)
+    #data["IND_INC_AVG"] = data["IND_INC_AVG"].astype(float)
+    #data["GRAD_DEBT_MDN"] = data["GRAD_DEBT_MDN"].astype(float)
+
+    #data = data.dropna()
+
+    #lowercase = lambda x: str(x).lower()
+    #data.rename(lowercase, axis='columns', inplace=True)
+
     return data
+
+df_data = load_data()
+
+#st.map(df_data)
